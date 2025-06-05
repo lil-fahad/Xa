d85c507a27cabbae9d91f22871b89692595877bb
lil-fahad <73719703+lil-fahad@users.noreply.github.com>
2025-06-05 04:02:36 +0300

    Delete ar_module/launch_streamlit.py

--- ar_module/launch_streamlit.py
+++
@@ -1,7 +0,0 @@
-import os
-import subprocess
-
-print("🚀 Launching Streamlit app from ar_module...")
-
-os.environ["STREAMLIT_BROWSER_GATHER_USAGE_STATS"] = "false"
-subprocess.run(["streamlit", "run", "ar_module/streamlit_ar_app.py"])
